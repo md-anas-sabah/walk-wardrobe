515105600612eb70752d74a6b056f3962cdcf6db
Md Anas Sabah <mdanassabah@gmail.com>
2023-10-19 00:30:54 +0530

    created navbar

---
+++ src/utils/index.js
@@ -0,0 +1,45 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listing",
+    label: "All Products",
+    path: "/product/listing/all-products",
+  },
+  {
+    id: "listingMen",
+    label: "Men",
+    path: "/product/listing/men",
+  },
+  {
+    id: "listingWomen",
+    label: "Women",
+    path: "/product/listing/women",
+  },
+  {
+    id: "listingKids",
+    label: "kids",
+    path: "/product/listing/kids",
+  },
+];
+
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Products",
+    path: "/admin-view/all-products",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Product",
+    path: "/admin-view/add-product",
+  },
+];
+
+export const styles = {
+  button:
+    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium rounded-lg uppercase tracking-wide  text-white hover:bg-gray-700",
+};

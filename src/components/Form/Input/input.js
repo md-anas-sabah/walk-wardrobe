30d1b6a85e9b54dca5c542897fa6e4c2cccae8d6
Md Anas Sabah <mdanassabah@gmail.com>
2023-10-24 01:42:41 +0530

    created input and select component

---
+++ src/components/Form/Input/input.js
@@ -0,0 +1,22 @@
+export default function InputComponent({
+  label,
+  placeholder,
+  type,
+  value,
+  onChange,
+}) {
+  return (
+    <div className="relative">
+      <p className="pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600">
+        {label}
+      </p>
+      <input
+        placeholder={placeholder}
+        type={type || "text"}
+        value={value}
+        onChange={onChange}
+        className="border placeholder-gray-400 focus:outline-none focus:border-black w-full pt-4 pl-4 pr-4 pb-4 mr-0 mt-0 ml-0 text-base block bg-white border-gray-300 rounded-md"
+      />
+    </div>
+  );
+}

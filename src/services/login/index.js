2d2afdc6a41b7a2c2dee0be61b0498707f66c677
Md Anas Sabah <mdanassabah@gmail.com>
2023-11-10 08:45:24 +0530

    added service for login page

---
+++ src/services/login/index.js
@@ -0,0 +1,16 @@
+export const login = async (formData) => {
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};

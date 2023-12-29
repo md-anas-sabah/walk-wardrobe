81593832fd8e4369b7ea7b562b38a405ee2eaf1f
Md Anas Sabah <mdanassabah@gmail.com>
2023-12-29 23:17:34 +0530

    fixing cors issue in cart

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -22,7 +22,7 @@ export const addToCart = async (formData) => {
 export const getAllCartItems = async (id) => {
   try {
     const res = await fetch(
-      `http://localhost:3000/api/cart/all-cart-items?id=${id}`,
+      `https://walk-wardrobe.vercel.app/api/cart/all-cart-items?id=${id}`,
       {
         method: "GET",
         headers: {

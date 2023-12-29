f021e454492cd372c7bce647cd986d1e89809193
Md Anas Sabah <mdanassabah@gmail.com>
2023-12-30 01:37:57 +0530

    fixing issues

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -138,6 +138,7 @@ export default function Checkout() {
       },
       quantity: 1,
     }));
+    console.log(createLineItems)
 
     const res = await callStripeSession(createLineItems);
     setIsOrderProcessing(true);
